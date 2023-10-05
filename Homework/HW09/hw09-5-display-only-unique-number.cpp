a847873f383cea8a5a32222d1e0d7f83c0f977f9
Biwane20 <biwbew20@gmail.com>
2023-10-05 13:41:40 +0700

    9-5

--- Homework/HW09/hw09-5-display-only-unique-number.cpp
+++ Homework/HW09/hw09-5-display-only-unique-number.cpp
@@ -1,6 +1,6 @@
 /*
     ผู้ใช้กรอกจำนวนอาเรย์ที่ต้องการกรอก จากนั้นผู้ใช้ทำการกรอกข้อมูลลงไปในอาเรย์จนครบ และให้คุณแสดงค่าในอาเรย์ที่ไม่มีการซ้ำกันเท่านั้น โดยแสดงผลลัพธ์จากน้อยไปมาก
-    
+
     Test case:
         Input N :
             5
@@ -17,7 +17,7 @@
     Output:
         Unique value : 2
 
-    
+
     Test case:
         Input N :
             6
@@ -37,3 +37,53 @@
         Unique value : 1 2 5 6
 
 */
+#include <stdio.h>
+int main()
+{
+    int N, same, hold = 0;
+    int arr[100];
+    int not_same;
+
+    printf("Input N :\n");
+    scanf("%d", &N);
+
+    for (int i = 0; i < N; i++)
+    {
+        printf("Input :\n");
+        scanf("%d", &arr[i]);
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = i + 1; j < N; j++)
+        {
+            if (arr[i] > arr[j])
+            {
+                hold = arr[j];
+                arr[j] = arr[i];
+                arr[i] = hold;
+            }
+        }
+    }
+
+    printf("Unique value :");
+
+    for (int i = 0; i < N; i++)
+    {
+        int same = 1;
+        while (i < N - 1 && arr[i] == arr[i + 1])
+        {
+            i++;
+            same++;
+        }
+
+        if (same == 1)
+        {
+            printf(" %d", arr[i]);
+        }
+    }
+
+    printf("\n");
+
+    return 0;
+}
